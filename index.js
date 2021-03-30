54b1a9d1d62f9c6276b9ce1b084435e3e7f57c53
Madalin Cosmin <madalinvasile91@gmail.com>
2021-03-30 15:37:44 +0300

    Updates

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {BrandsContainer} from './BrandsElements'
+
+const Brands = () => {
+    return (
+        <BrandsContainer>
+            <div><img src="../../assets/images/nike.png" alt=""/></div>
+            <div><img src="" alt=""/></div>
+            <div><img src="" alt=""/></div>
+        </BrandsContainer>
+    )
+}
+
+export default Brands
